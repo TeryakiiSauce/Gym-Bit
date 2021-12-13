882528b0a519a3c59e3258c76e3a8978863ccb55
Ali Alqallaf <allimazen7@gmail.com>
2021-12-13 12:40:07 +0300

    initial commit

---
+++ Group3_WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group3_WorkoutApp
+//
+//  Created by Ali Alqallaf on 13/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
