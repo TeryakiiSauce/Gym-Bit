5f70b0e4506d74a6aefe00d755ea30a98468d129
Ali Alqallaf <allimazen7@gmail.com>
2021-12-30 05:45:44 +0300

    create folder to hold play workout view controllers, create time picker, create animated circular progress indicator for the timer, pass selected time to the timer screen

---
+++ Group3_WorkoutApp/ViewControllers/PlayWorkout/PlayWorkoutLandingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LandingPlayWorkoutViewController.swift
+//  Group3_WorkoutApp
+//
+//  Created by Ali Alqallaf on 29/12/2021.
+//
+
+import UIKit
+import SwiftUI
+
+class LandingPlayWorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
