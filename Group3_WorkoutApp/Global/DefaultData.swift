62c7cf53e24dc7215cd12aa8b6d661dce78ca7a5
Abdulaziz-Hasan <201802598@student.polytechnic.bh>
2022-01-01 21:55:00 +0300

    added comments to defaultData class

--- Group3_WorkoutApp/Global/DefaultData.swift
+++ Group3_WorkoutApp/Global/DefaultData.swift
@@ -9,42 +9,48 @@ import Foundation
 
 struct DefaultData {
     
+    //instance of user
     static var user = User(dateJoined: Date(), fullName: "Mike Gonzalis", height: 177, weight: 68, goalWeight: 70, dailyWorkoutMinutes: nil, dailyCardioMinutes: nil, avgWeeklyWorkoutMinutes: nil, avgWeeklyCardioMinutes: nil, totalyWorkoutMinutes: nil, totalCardioMinutes: nil, totalWorkedoutDays: nil, workoutStreak: nil, schedules: schedules, activeSchedule: schedules[0])
     
+    //default instance of exercises
     static var backExercises =
         [
-            
             Exercise(name: "Pull down", description: "description", imagePath: "back2", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.back.rawValue)
         ]
+    
     static var chestExercises =
         [
             Exercise(name: "Chest bench Press", description: "description", imagePath: "chest1", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.chest.rawValue)
         ]
+    
     static var shoulderExercises =
         [
-           
             Exercise(name: "Shoulder Press", description: "description", imagePath: "shoulder1", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.shoulders.rawValue)
         ]
+    
     static var legExercises =
         [
             Exercise(name: "Leg Press", description: "description", imagePath: "legs1", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.legs.rawValue)
         ]
+    
     static var absExercises =
         [
-            
             Exercise(name: "Crunches", description: "description", imagePath: "abs1", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.abs.rawValue)
         ]
+    
     static var bicepExercises =
         [
-            
             Exercise(name: "Curls", description: "description", imagePath: "bi2", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.biceps.rawValue)
         ]
     
     static var tricepExercises =
         [
-            
             Exercise(name: "Rope", description: "description", imagePath: "tri1", tips: ["tip1","tip2"], targetMuscle: Exercise.TargetMuscle.triceps.rawValue)
         ]
+    
+    
+    
+    // creating a default schedule
     static var schedules =
         [Schedule(dateCreated: Date(), name: "Full Body Workout", playsCounter: 0,exercises: [tricepExercises[0],bicepExercises[0],shoulderExercises[0],backExercises[0],chestExercises[0],legExercises[0],absExercises[0]])]
     
