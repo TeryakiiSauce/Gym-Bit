425511cea2034401c23f6248d4b233b070f92ab0
Abdulaziz-Hasan <201802598@student.polytechnic.bh>
2022-01-03 23:21:11 +0300

    fixed AppColors and modified manageSchedule view controllers

--- Group3_WorkoutApp/Models/Exercise.swift
+++ Group3_WorkoutApp/Models/Exercise.swift
@@ -9,12 +9,24 @@ import Foundation
 
 struct Exercise: Codable {
     
-    let id = UUID()
+    var id = UUID()
     let name: String
     let description: String
     let imagePath: String
     let tips: [String]
     let targetMuscle: String
+    var reps: String {
+        switch targetMuscle {
+        case "back","chest":
+            return "12 reps x 3 sets"
+        case "biceps","triceps","shoulders":
+            return "10 reps x 3 sets"
+        case "abs","legs":
+            return "15 reps x 4 sets"
+        default:
+            return "10 reps x 3 sets"
+        }
+    }
     
     enum TargetMuscle: String {
         case back = "back"
