576eae4d1e7f334db939561f841fac1851b39a5c
Albaraa Janahi <201800452@student.polytechnic.bh>
2022-01-04 10:07:29 +0300

    working on quiz

--- Group3_WorkoutApp/ViewControllers/QuizViewController.swift
+++ Group3_WorkoutApp/ViewControllers/QuizViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    @IBOutlet weak var Line1: UILabel!
+    @IBOutlet weak var Line2: UILabel!
     @IBOutlet weak var bodyView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Setting the default style to the view and the body view by calling a funcation from Constants struct.
         Constants.applyDefaultStyling(backgroundView: view, headerView: nil, bodyView: bodyView, mainButton: nil, secondaryButton: nil)
+        // Changing colors to match the app theme by calling AppColors stuct.
         bodyView.backgroundColor = AppColors.bodyBg
+        Line1.textColor = AppColors.phoneBg
+        Line2.textColor = AppColors.phoneBg
         // Do any additional setup after loading the view.
     }
     
