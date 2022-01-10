0d1f4d40b46f6b2cf605bd31395217fef5316d83
Albaraa Janahi <201800452@student.polytechnic.bh>
2022-01-11 00:16:43 +0300

    Working on settings page

--- Group3_WorkoutApp/ViewControllers/SettingsViewController.swift
+++ Group3_WorkoutApp/ViewControllers/SettingsViewController.swift
@@ -15,6 +15,7 @@ class SettingsViewController: UIViewController {
     
     let userDefaults = UserDefaults()
 
+    @IBOutlet weak var backView: UIView!
     @IBOutlet weak var headView1: UIView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var slider1: UISlider!
@@ -30,13 +31,26 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var headLabel: UILabel!
     @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var headView2: UIView!
+    @IBOutlet weak var appSettingsLabel: UILabel!
+    @IBOutlet weak var settingsView2: UIView!
+    @IBOutlet weak var timeSoundTxt: UILabel!
+    @IBOutlet weak var darkModeTxt: UILabel!
+    @IBOutlet weak var unitTxt: UILabel!
+    @IBOutlet weak var soundSwitch: UIImageView!
+    @IBOutlet weak var darkModeSwitch: UIImageView!
+    @IBOutlet weak var unitSwitch: UIImageView!
+    @IBOutlet weak var line1: UILabel!
+    @IBOutlet weak var line2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Setting the default styles to the page elements by calling a funcation from Constants struct.
         Constants.applyDefaultStyling(backgroundView: view, headerView: headView1, bodyView: settingsView, mainButton: saveButton, secondaryButton: nil)
+        Constants.buildRoundedUIView(headerView: headView2, bodyView: settingsView2, button: nil)
         scrollView.backgroundColor = AppColors.phoneBg
+        backView.backgroundColor = AppColors.phoneBg
         
-        // Changing colors to match the app theme by calling AppColors stuct.
+        // Changing view 1 elements colors to match the app theme by calling AppColors stuct.
         headLabel.textColor = AppColors.textColor
         nameField.textColor = AppColors.phoneBg
         heightTxt.textColor = AppColors.textColor
@@ -52,7 +66,12 @@ class SettingsViewController: UIViewController {
         slider3.thumbTintColor = AppColors.buttonColor
         slider3.tintColor = AppColors.buttonColor
         
-        
+        // Changing view 2 elements colors to match the app theme by calling AppColors stuct.
+        timeSoundTxt.textColor = AppColors.textColor
+        darkModeTxt.textColor = AppColors.textColor
+        unitTxt.textColor = AppColors.textColor
+        line1.textColor = AppColors.phoneBg
+        line2.textColor = AppColors.phoneBg
         //Checking if name key is not nil, if it is not it means that the user did the quiz so we will retrieve the data.
         if let usrName = userDefaults.value(forKey: "name") as? String
         {
@@ -79,6 +98,14 @@ class SettingsViewController: UIViewController {
         heightValue.text = getHeight()
         weightValue.text = getWeight()
         goalValue.text = getGoal()
+    }
+    @IBAction func fieldEditted(_ sender: UITextField)
+    {
+        if (sender.text == "Your name")
+        {
+            sender.text = "";
+            sender.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        }
     }
     @IBAction func sliderMoved(_ sender: UISlider)
     {
@@ -142,6 +169,62 @@ class SettingsViewController: UIViewController {
         return goal
     }
 
+    @IBAction func saveClicked(_ sender: Any)
+    {
+        if (nameField.text == "Your name" || nameField.text == "")
+        {
+            Constants.displayAlert(thisClass: self, alertTitle: "Alert", msg: "Please enter your name!", printInConsole: nil)
+            return
+        }
+        
+        //validating if very short name or only spaces.
+        var name = nameField.text!
+        name = name.trimmingCharacters(in: .whitespaces)
+        if(name == "")
+        {
+            Constants.displayAlert(thisClass: self, alertTitle: "Alert", msg: "Please enter your name!", printInConsole: nil)
+            return
+        }
+        if(name.count < 3)
+        {
+            Constants.displayAlert(thisClass: self, alertTitle: "Alert", msg: "Your name should be at least three letters", printInConsole: nil)
+            return
+        }
+        
+        //validating if name contain any number or special charecter, also counting spaces if more than one.
+        var numOfSpaces = 0
+        for l in name
+        {
+            if (!l.isLetter)
+            {
+                if(!l.isWhitespace)
+                {
+                    Constants.displayAlert(thisClass: self, alertTitle: "Alert", msg: "Your name can contain only letters and a white space", printInConsole: nil)
+                    return
+                }
+                else
+                {
+                    numOfSpaces += 1
+                }
+            }
+        }
+        if numOfSpaces > 1
+        {
+            Constants.displayAlert(thisClass: self, alertTitle: "Alert", msg: "The name text field can contain only one white space (between first and last name)", printInConsole: nil)
+            return
+        }
+        
+        //Since the code reached hear it means that the input passed all the validations.
+        //Saving the data to retrieve it later.
+        let height = Int(slider1.value)
+        let weight = Int(slider2.value)
+        let goal = Int(slider3.value)
+        userDefaults.setValue(name, forKey: "name")
+        userDefaults.setValue(height, forKey: "height")
+        userDefaults.setValue(weight, forKey: "weight")
+        userDefaults.setValue(goal, forKey: "goal")
+        Constants.displayAlert(thisClass: self, alertTitle: "Done", msg: "Your data updated successfully", printInConsole: nil)
+    }
     /*
     // MARK: - Navigation
 
